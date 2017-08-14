12e6dd4965f3522bece95efc1f5a4d1a9ccf68df
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-08-14 20:31:16 +0100

    adding a register page - part 3

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,19 @@
 var bodyParser = require('body-parser')
+var usersControllers = require('../controllers/usersControllers.js')
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 function userRoutes(app){
     app.get('/', function(req, res) {
         res.render('home', {currentUser: req.session.user})
+    })
+
+    app.get('/register', function(req, res) {
+        res.render('register')
+    })
+
+    app.post('/register', urlencodedParser, usersControllers.register, function (req, res){
+        res.redirect('/')
     })
 }
 
